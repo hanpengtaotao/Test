5379816ae334d74774924e060215a2eafb8439b8
韩朋涛 <2008hanpengtao@163.com>
2020-01-11 22:53:24 +0800

    新增hello.java中的字段

--- src/main/java/hello.java
+++ src/main/java/hello.java
@@ -1,2 +1,21 @@
 public class hello {
+    private String name;
+
+    private Interger age;
+
+    public Interger getAge() {
+        return age;
+    }
+
+    public void setAge(Interger age) {
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
